4dd4fae143af78da08bf77405a45ae207c1f784a
rcacacho <estuardo.cacacho@gmail.com>
2024-01-08 10:42:57 -0600

    url

--- src/environments/environment.js
+++ src/environments/environment.js
@@ -1,5 +1,5 @@
 const environment = {
-    baseApiUrl: 'https://biblioleo-700ad891664c.herokuapp.com/'
+    baseApiUrl: 'https://biblioleo-700ad891664c.herokuapp.com'
 }
 
 export default environment;
